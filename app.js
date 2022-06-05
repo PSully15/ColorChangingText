90423bfd90b7ecd2e0e7d5742eb4db2b357fbb21
Patrick Sullivan <patricksullivan797@gmail.com>
2022-06-05 11:34:54 -0400

    Added content to js file

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+    r = Math.floor(Math.random * 256);
+    g = Math.floor(Math.random * 256);
+    b = Math.floor(Math.random * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function() {
+    for (let letter of letters) {
+        letter.style.color = randomRGB();
+    }
+});
